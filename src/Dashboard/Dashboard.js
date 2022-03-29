48821bd7b6fbdd33b060d6416c4b66ba147b0eff
Jeffrey Cherewaty <jeffrey@cherewaty.com>
2022-03-29 16:45:48 -0500

    Fix the footer in place

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -3,9 +3,11 @@ import { styled } from "@mui/material/styles";
 import {
   AppBar,
   Box,
+  Button,
   Container,
   Drawer,
   IconButton,
+  Stack,
   TextField,
   Toolbar,
   Typography,
@@ -126,6 +128,8 @@ function DashboardContent() {
         sx={{
           flexGrow: 1,
           overflow: "auto",
+          display: "flex",
+          flexDirection: "column",
         }}
       >
         <AppBar
@@ -154,82 +158,95 @@ function DashboardContent() {
             </Typography>
           </Toolbar>
         </AppBar>
-        <Container>
-          <Box component="div" sx={{ marginBottom: 4 }}>
-            <TextField label="Enter text" variant="standard" />
-          </Box>
-          <Box>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-              vehicula nibh quis leo dictum, nec feugiat ante vestibulum. Sed
-              nec tempus elit. Vestibulum ante ipsum primis in faucibus orci
-              luctus et ultrices posuere cubilia curae; Quisque quis orci
-              rhoncus turpis porttitor pellentesque. Vivamus consequat efficitur
-              blandit. Morbi egestas convallis elit vitae gravida. Etiam posuere
-              lorem a feugiat lobortis. Maecenas at tellus sem. Vestibulum enim
-              erat, pretium sit amet tempor vel, auctor sed ipsum. Ut vitae
-              libero et turpis facilisis aliquet a quis nisi. Quisque sed elit
-              auctor, consequat mi ac, maximus orci. Fusce a odio purus.
-              Phasellus rhoncus lacus eu porta facilisis. Orci varius natoque
-              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-            </p>
-            <p>
-              Donec turpis ligula, euismod et tempus nec, ultrices et nibh. Nunc
-              ut mi quam. Phasellus at diam leo. Duis laoreet nunc id luctus
-              pulvinar. Etiam efficitur, justo vitae vulputate pretium, ipsum
-              metus eleifend justo, eu dictum leo mauris non sapien. Aliquam a
-              dignissim eros. Orci varius natoque penatibus et magnis dis
-              parturient montes, nascetur ridiculus mus. Vestibulum sem odio,
-              bibendum eu sapien sed, mattis posuere augue. Etiam vestibulum
-              mollis bibendum. Nulla nec tortor suscipit, maximus nibh et,
-              blandit sem. Integer orci libero, sollicitudin eu justo sit amet,
-              tincidunt tempor diam.
-            </p>
-            <p>
-              Duis vitae posuere lacus, quis facilisis nisi. Nunc quis nulla non
-              ligula congue hendrerit. Suspendisse accumsan tortor non metus
-              condimentum, a ultrices dui porta. Aenean imperdiet volutpat
-              augue, vitae suscipit velit gravida non. Nullam risus libero,
-              finibus eget nunc in, molestie auctor augue. Etiam eu justo purus.
-              Sed eget commodo metus, sodales volutpat magna. Aenean molestie,
-              tortor nec elementum tempus, lorem arcu posuere arcu, eu volutpat
-              nulla lacus vel odio. Vivamus fringilla erat nisi, consequat
-              laoreet est eleifend in. Vivamus hendrerit blandit augue at
-              finibus. Maecenas iaculis velit quis facilisis porta. Curabitur
-              sit amet mi elit.
-            </p>
-            <p>
-              Duis sit amet felis at tortor luctus ullamcorper. Proin nec justo
-              eget augue feugiat convallis vulputate non ante. Quisque non
-              mollis nulla. Fusce lacinia blandit lacus quis aliquam. Vestibulum
-              ante justo, pretium nec metus ac, lobortis posuere nibh. Etiam
-              consequat mattis justo, vitae consequat eros maximus interdum. Sed
-              rutrum auctor est, sit amet semper libero pellentesque id. Nam
-              facilisis diam ut augue pretium, sed euismod risus tempor. Aenean
-              sagittis, odio ut feugiat fermentum, arcu lectus lobortis justo,
-              vel semper magna erat ac mi. Vivamus eu dolor in ipsum lobortis
-              bibendum. Curabitur massa est, mattis a tempor nec, commodo non
-              dui. Curabitur sit amet posuere est. Nulla vel arcu mollis, mattis
-              elit a, porta magna. Pellentesque erat nulla, viverra laoreet
-              augue in, lacinia consectetur turpis.
-            </p>
-            <p>
-              Sed hendrerit felis quam, sed hendrerit erat ullamcorper vel.
-              Vestibulum nibh metus, gravida id sapien ut, placerat luctus
-              lacus. Fusce rhoncus felis id justo tincidunt, ac condimentum
-              metus hendrerit. Cras vitae malesuada nunc. Curabitur a
-              pellentesque orci, et auctor nulla. Cras vel turpis velit. Duis id
-              ligula at libero accumsan porta in ut dui. Vivamus vitae lacus
-              congue, ornare dui eu, luctus massa. Sed erat massa, elementum id
-              lacinia quis, tincidunt ac justo. In facilisis sed odio sed
-              pulvinar. Interdum et malesuada fames ac ante ipsum primis in
-              faucibus. Sed a tristique mi, quis ullamcorper quam. Cras tempus
-              odio turpis, nec euismod urna vehicula viverra. Quisque id ante
-              augue. In congue, urna placerat suscipit pulvinar, nunc ipsum
-              vestibulum ante, quis dapibus ex sapien ut massa.
-            </p>
-          </Box>
-        </Container>
+        <Box component="div" sx={{ overflowY: "scroll" }}>
+          <Container>
+            <Box component="div" sx={{ marginBottom: 4 }}>
+              <TextField label="Enter text" variant="standard" />
+            </Box>
+            <Box>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                vehicula nibh quis leo dictum, nec feugiat ante vestibulum. Sed
+                nec tempus elit. Vestibulum ante ipsum primis in faucibus orci
+                luctus et ultrices posuere cubilia curae; Quisque quis orci
+                rhoncus turpis porttitor pellentesque. Vivamus consequat
+                efficitur blandit. Morbi egestas convallis elit vitae gravida.
+                Etiam posuere lorem a feugiat lobortis. Maecenas at tellus sem.
+                Vestibulum enim erat, pretium sit amet tempor vel, auctor sed
+                ipsum. Ut vitae libero et turpis facilisis aliquet a quis nisi.
+                Quisque sed elit auctor, consequat mi ac, maximus orci. Fusce a
+                odio purus. Phasellus rhoncus lacus eu porta facilisis. Orci
+                varius natoque penatibus et magnis dis parturient montes,
+                nascetur ridiculus mus.
+              </p>
+              <p>
+                Donec turpis ligula, euismod et tempus nec, ultrices et nibh.
+                Nunc ut mi quam. Phasellus at diam leo. Duis laoreet nunc id
+                luctus pulvinar. Etiam efficitur, justo vitae vulputate pretium,
+                ipsum metus eleifend justo, eu dictum leo mauris non sapien.
+                Aliquam a dignissim eros. Orci varius natoque penatibus et
+                magnis dis parturient montes, nascetur ridiculus mus. Vestibulum
+                sem odio, bibendum eu sapien sed, mattis posuere augue. Etiam
+                vestibulum mollis bibendum. Nulla nec tortor suscipit, maximus
+                nibh et, blandit sem. Integer orci libero, sollicitudin eu justo
+                sit amet, tincidunt tempor diam.
+              </p>
+              <p>
+                Duis vitae posuere lacus, quis facilisis nisi. Nunc quis nulla
+                non ligula congue hendrerit. Suspendisse accumsan tortor non
+                metus condimentum, a ultrices dui porta. Aenean imperdiet
+                volutpat augue, vitae suscipit velit gravida non. Nullam risus
+                libero, finibus eget nunc in, molestie auctor augue. Etiam eu
+                justo purus. Sed eget commodo metus, sodales volutpat magna.
+                Aenean molestie, tortor nec elementum tempus, lorem arcu posuere
+                arcu, eu volutpat nulla lacus vel odio. Vivamus fringilla erat
+                nisi, consequat laoreet est eleifend in. Vivamus hendrerit
+                blandit augue at finibus. Maecenas iaculis velit quis facilisis
+                porta. Curabitur sit amet mi elit.
+              </p>
+              <p>
+                Duis sit amet felis at tortor luctus ullamcorper. Proin nec
+                justo eget augue feugiat convallis vulputate non ante. Quisque
+                non mollis nulla. Fusce lacinia blandit lacus quis aliquam.
+                Vestibulum ante justo, pretium nec metus ac, lobortis posuere
+                nibh. Etiam consequat mattis justo, vitae consequat eros maximus
+                interdum. Sed rutrum auctor est, sit amet semper libero
+                pellentesque id. Nam facilisis diam ut augue pretium, sed
+                euismod risus tempor. Aenean sagittis, odio ut feugiat
+                fermentum, arcu lectus lobortis justo, vel semper magna erat ac
+                mi. Vivamus eu dolor in ipsum lobortis bibendum. Curabitur massa
+                est, mattis a tempor nec, commodo non dui. Curabitur sit amet
+                posuere est. Nulla vel arcu mollis, mattis elit a, porta magna.
+                Pellentesque erat nulla, viverra laoreet augue in, lacinia
+                consectetur turpis.
+              </p>
+              <p>
+                Sed hendrerit felis quam, sed hendrerit erat ullamcorper vel.
+                Vestibulum nibh metus, gravida id sapien ut, placerat luctus
+                lacus. Fusce rhoncus felis id justo tincidunt, ac condimentum
+                metus hendrerit. Cras vitae malesuada nunc. Curabitur a
+                pellentesque orci, et auctor nulla. Cras vel turpis velit. Duis
+                id ligula at libero accumsan porta in ut dui. Vivamus vitae
+                lacus congue, ornare dui eu, luctus massa. Sed erat massa,
+                elementum id lacinia quis, tincidunt ac justo. In facilisis sed
+                odio sed pulvinar. Interdum et malesuada fames ac ante ipsum
+                primis in faucibus. Sed a tristique mi, quis ullamcorper quam.
+                Cras tempus odio turpis, nec euismod urna vehicula viverra.
+                Quisque id ante augue. In congue, urna placerat suscipit
+                pulvinar, nunc ipsum vestibulum ante, quis dapibus ex sapien ut
+                massa.
+              </p>
+            </Box>
+          </Container>
+        </Box>
+        <Toolbar sx={{ justifyContent: "flex-end" }}>
+          <Container>
+            <Stack direction="row-reverse" spacing={2}>
+              <Button variant="contained">Save</Button>
+              <Button variant="outlined">Cancel</Button>
+            </Stack>
+          </Container>
+        </Toolbar>
       </Box>
     </Box>
   );
