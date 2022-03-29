3acd739251164eb07343514325ad53810266b85c
Jeffrey Cherewaty <jeffrey@cherewaty.com>
2022-03-29 08:12:48 -0500

    Work on small up drawer transition

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -41,31 +41,31 @@ const AppBar = styled(MuiAppBar, {
   }),
 }));
 
-// const Drawer = styled(MuiDrawer, {
-//   shouldForwardProp: (prop) => prop !== "open",
-// })(({ theme, open }) => ({
-//   "& .MuiDrawer-paper": {
-//     position: "relative",
-//     whiteSpace: "nowrap",
-//     width: drawerWidth,
-//     transition: theme.transitions.create("width", {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//     boxSizing: "border-box",
-//     ...(!open && {
-//       overflowX: "hidden",
-//       transition: theme.transitions.create("width", {
-//         easing: theme.transitions.easing.sharp,
-//         duration: theme.transitions.duration.leavingScreen,
-//       }),
-//       width: theme.spacing(7),
-//       [theme.breakpoints.up("sm")]: {
-//         width: theme.spacing(9),
-//       },
-//     }),
-//   },
-// }));
+const SmallUpDrawer = styled(Drawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
 
 function DashboardContent(props) {
   const [xSmallBreakpointDrawerOpen, setXSmallBreakpointDrawerOpen] =
